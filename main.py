c06cf00080fd76ce18dda23a8fce8af12c0a18a3
NateClarke0 <n.austinclarke@ufl.edu>
2023-03-22 17:00:41 -0400

    added a main() function - Nathaniel Austin-Clarke

--- main.py
+++ main.py
@@ -11,24 +11,29 @@ def encoder(password):
     return encoded_password
 
     # encodes password by shifting all digits up by three
-
-def menu():
-    print("Menu")
-    print("-------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
-
-if __name__ == "__main__":
+def main():
     while True:
-        menu()
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
         option = int(input("\nPlease enter an option: "))
+
         if option == 1:
             password = int(input("Please enter your password to encode: "))
             password = encoder(str(password))
             print("Your password has been encoded and stored!\n")
         elif option == 2:
-            #FIXME: Add decoder option.
+            # FIXME: Add decoder option.
             raise NotImplementedError
         elif option == 3:
             break
+        else:
+            print("That is not an option.\n")
+            continue
+
+
+if __name__ == "__main__":
+    main()
